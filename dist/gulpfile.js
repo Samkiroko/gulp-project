47bb616c1660c1f92f0595687426cb77924e2866
SamKiroko <kirokodev@gmail.com>
2019-09-14 21:33:49 +0300

    gulp-project

---
+++ dist/gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+
+gulp.task('test-task', () => {
+
+});
+gulp.task('processHTML', () => {
+
+});
+gulp.task('processHTML', () => {
+  gulp.src('*.html')
+});
+gulp.task('processHTML', () => {
+  gulp.src('*.html')
+    .pipe(gulp.dest('dist'));
+});
+gulp.task('processJS', () => {
+  gulp.src('*.js')
+    .pipe(gulp.dest('dist'));
+});
